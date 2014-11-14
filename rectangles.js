686afd7c81ac110b101fc038f3415c5abbc2a7e7
David Gowrie <david.gowrie@gmail.com>
2014-11-14 16:15:06 -0500

    creating array with multiple models, loop through the array and for each model render and append to page

--- rectangles.js
+++ rectangles.js
@@ -1,6 +1,9 @@
 // Rectangles.js for Rectangles.html demo
+/* global Backbone */
 
 (function () {
+
+	'use strict';
 
 	var Rectangle = Backbone.Model.extend({});
 
@@ -53,18 +56,48 @@
 
 	});
 
-	var myRectangle = new Rectangle({
-		width: 100,
-		height: 60,
-		position: {
-			x: 300,
-			y: 150
-		},
-		color: '#c00'
+	var models = [
+
+		new Rectangle({
+			width: 100,
+			height: 60,
+			position: {
+				x: 300,
+				y: 150
+			},
+			color: '#c00'
+		}),
+		new Rectangle({
+			width: 25,
+			height: 280,
+			position: {
+				x: 600,
+				y: 75
+			},
+			color: '#0c0'
+		}),
+		new Rectangle({
+			width: 300,
+			height: 80,
+			position: {
+				x: 320,
+				y: 210
+			},
+			color: '#00c'
+		})				
+
+	];
+
+	// use underscore.js to access the models array, has an .each function - really a for loop over the array
+	_(models).each(function (model) {
+
+		$('#canvas').append(new RectangleView({model: model}).render().el);
+
 	});
 
-	var myView = new RectangleView({model: myRectangle});
 
-	$('#canvas').append(myView.render().el);
+	// var myView = new RectangleView({model: myRectangle});
+
+	// $('#canvas').append(myView.render().el);
 
 }());
